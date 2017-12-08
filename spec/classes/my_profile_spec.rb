553d544598676871eabea1bf86a26bd463246252
Brian Vanderbusch <chief@hackerhappyhour.com>
2017-12-08 11:27:42 -0600

    setup tests without eyaml data sources

--- spec/classes/my_profile_spec.rb
+++ spec/classes/my_profile_spec.rb
@@ -6,6 +6,14 @@ describe 'my_profile' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+
+      context 'when in alpha sector' do
+        let(:facts) do
+          super().merge({'sector' => 'alpha'})
+
+          it { is_expected.to compile }
+        end
+      end
     end
   end
 end
